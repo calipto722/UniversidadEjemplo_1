35bb96918aee81e6d4427e7034c28c55d0630254
calipto722 <141640050+calipto722@users.noreply.github.com>
2023-09-28 09:24:17 -0300

    pantallas en medio

--- src/UniversidadEjemplo/Vistas/Login.java
+++ src/UniversidadEjemplo/Vistas/Login.java
@@ -16,7 +16,7 @@ public class Login extends javax.swing.JFrame {
 
     public Login() {
         initComponents();
-       
+       this.setLocationRelativeTo(null);
     }
  boolean vista=true;
     
--- src/UniversidadEjemplo/Vistas/MenuPrincipal.java
+++ src/UniversidadEjemplo/Vistas/MenuPrincipal.java
@@ -18,7 +18,7 @@ public class MenuPrincipal extends javax.swing.JFrame {
      */
     public MenuPrincipal() {
         initComponents();
-        
+        this.setLocationRelativeTo(null);
     }
 
     /**
